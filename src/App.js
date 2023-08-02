032237a45c0d006e84de0325fa10ac09183d5e4f
diksha2150487 <130853102+diksha2150487@users.noreply.github.com>
2023-08-02 21:28:49 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import img3 from "./image/Frame (1).png";
 import img4 from "./image/Vector (16).png";
 import img5 from "./image/check 1.png";
 import img6 from "./image/Group 72887.png";
+import img7 from "./image/Group 71310.png";
+import img8 from "./image/Group 71314.png";
+import img9 from "./image/Group 71318.png";
+import img10 from "./image/Group 71321.png";
 
 import { AiOutlineArrowRight } from "react-icons/ai";
 function App() {
@@ -100,6 +104,91 @@ function App() {
         </div>
         <div class="rightcontainer8">
           <img src={img6} />
+        </div>
+      </div>
+      {/* *************************************************** */}
+      <div class="cont-process">
+        <p className="plogy">THE PROCESS</p>
+        <div class="onerow">
+          <div className="secondrow" id="boximg">
+            <p className="need" id="dolt">
+              Determining
+            </p>
+            <p className="bord" id="liam">
+              Need
+            </p>
+            <img src={img7} id="joo" />
+          </div>
+          <div class="secondrow" id="lia">
+            <p className="need" id="toll">
+              Sourcing{" "}
+            </p>
+            <p className="bord" id="walt">
+              Talent
+            </p>
+            <img src={img8} id="rob" />
+          </div>
+          <div class="secondrow" id="flok">
+            <p className="need" id="hell">
+              On{" "}
+            </p>
+            <p className="bord" id="mist">
+              Boarding
+            </p>
+            <img src={img9} id="eko" />
+          </div>
+          <div class="secondrow" id="kylo">
+            <p className="need" id="volo">
+              Get Your Projects
+            </p>
+            <p className="bord" id="faq-lo">
+              {" "}
+              Delivered
+            </p>
+            <img src={img10} id="funny" />
+          </div>
+        </div>
+      </div>
+      {/* ******************************************************** */}
+      <div className="container-c">
+        <div class="left-row">
+          <h1>Contact Us</h1>
+          <h4>Please complete the below form. we will get in touch with you</h4>
+          <div class="input-container">
+            <div class="styled-input wide">
+              <input class="inp" type="text" placeholder="Name*" required />
+              <input
+                class="inp"
+                type="text"
+                placeholder="Phone number*"
+                required
+              />
+            </div>
+            <div class="styled-input wide">
+              <input
+                class="inp-1"
+                type="email"
+                placeholder="Email Address*"
+                required
+              />
+              <input
+                class="inp-1"
+                type="text"
+                placeholder="Location*"
+                required
+              />
+            </div>
+            <div class="styled-input wide">
+              <input class="inp-2" required placeholder="Business Name"></input>
+            </div>
+            <div class="styled-input wide">
+              <input class="inp-3" required placeholder="Message"></input>
+            </div>
+          </div>
+          <br />
+          <div id="form-btn" class="btn-lrg submit-btn">
+            Submit <i class="fa-solid fa-arrow-right" id="submit-right"></i>
+          </div>
         </div>
       </div>
     </div>
